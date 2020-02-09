fbb3885fe8f661966f8df1dd1f6b023e0d598f15
IgnacioAlbarran <ignacioalbarran8512@gmail.com>
2020-02-09 17:43:08 +0100

    Add route_path

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   get 'web/home'
   get 'web/about_us'
   get 'web/search'
+
+  root to: 'web#home'
+
   resources :books
   resources :authors
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
