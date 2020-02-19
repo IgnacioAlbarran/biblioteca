a561c9680a296c1265df2652f1d6f10aace8f103
IgnacioAlbarran <ignacioalbarran8512@gmail.com>
2020-02-19 14:15:28 +0100

    Add variant to home page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+  before_action :set_variant
+
+  def set_variant
+    return unless params[:variant].in?(%w(phone desktop tablet))
+    request.variant = params[:variant].to_sym
+  end
 end
