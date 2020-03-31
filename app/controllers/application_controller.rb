0506cc685fb9223d2b68ebb8267d7323a97a3eb7
Ignacio Albarran <ignacioalbarran8512@gmail.com>
2020-03-31 17:22:42 +0200

    change languages YML files names add link_to for language swapping

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :set_variant
+  before_action :set_locale
 
   def set_variant
     request.variant =
