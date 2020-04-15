66c1262e87a7c5d565df14373f7334dd35269481
Ignacio Albarran <ignacioalbarran8512@gmail.com>
2020-04-16 00:37:00 +0200

    stub REDIRECTED_AUTHORS constant to test just functionallity and change constant values to avoid malfunction

--- app/models/author.rb
+++ app/models/author.rb
@@ -3,7 +3,7 @@ class Author < ApplicationRecord
   has_many :books
   validates :email, :first_name, presence: true
 
-  REDIRECTED_AUTHORS = { '2' => 5, '3' => 6 }
+  REDIRECTED_AUTHORS = { '122' => 132, '125' => 135 }
 
   def to_s
     "#{first_name} #{last_name}"
--- spec/controllers/authors_controller_spec.rb
+++ spec/controllers/authors_controller_spec.rb
@@ -139,8 +139,9 @@ RSpec.describe AuthorsController, type: :controller do
   end
 
   describe 'redirect to author' do
-    let(:new_author) { 5 }
-    let(:old_author) { 2 }
+    let(:new_author) { 444 }
+    let(:old_author) { '333' }
+    before { stub_const('Author::REDIRECTED_AUTHORS', { old_author => new_author }) }
 
     it "should redirect from the old author to the new one" do
       get :show, params: { id: old_author }
